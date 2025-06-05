c6ab7336d502038476a403f90fd323e6462792ca
fatbobman <xuyang@me.com>
2025-06-05 08:32:50 +0800

    Add VSCode settings, Swift file snippets, and MIT License; update Package.swift dependencies

--- Package.swift
+++ Package.swift
@@ -4,21 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "Objects2XLSX",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Objects2XLSX",
-            targets: ["Objects2XLSX"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Objects2XLSX"),
-        .testTarget(
-            name: "Objects2XLSXTests",
-            dependencies: ["Objects2XLSX"]
-        ),
-    ]
+  name: "Objects2XLSX",
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "Objects2XLSX",
+      targets: ["Objects2XLSX"],
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/tadija/AEXML.git", from: "4.7.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "Objects2XLSX",
+      dependencies: [
+        .product(name: "AEXML", package: "AEXML"),
+      ],
+    ),
+    .testTarget(
+      name: "Objects2XLSXTests",
+      dependencies: ["Objects2XLSX"],
+    ),
+  ],
+  swiftLanguageModes: [.v6],
 )
