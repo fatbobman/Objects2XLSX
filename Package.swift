0925a260939fdd151223e2914c02483a98000fcb
fatbobman <xuyang@me.com>
2025-06-05 08:04:53 +0800

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Objects2XLSX",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Objects2XLSX",
+            targets: ["Objects2XLSX"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Objects2XLSX"),
+        .testTarget(
+            name: "Objects2XLSXTests",
+            dependencies: ["Objects2XLSX"]
+        ),
+    ]
+)
