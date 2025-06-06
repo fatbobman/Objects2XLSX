fb3780fa3d6b114b8d1fd296e32a878ee720d60e
fatbobman <xuyang@me.com>
2025-06-06 14:44:44 +0800

    Comment out the Sheet struct and its initializers for future reference

--- Sources/Objects2XLSX/Core/Sheet.swift
+++ Sources/Objects2XLSX/Core/Sheet.swift
@@ -1,20 +1,30 @@
-public struct Sheet<ObjectType> {
-    public let name: String
-    public let columns: [AnyColumn<ObjectType>]
-    public let data: [ObjectType]
+//
+// Sheet.swift
+// Created by Xu Yang on 2025-06-06.
+// Blog: https://fatbobman.com
+// GitHub: https://github.com/fatbobman
+//
+// Copyright © 2025 Fatbobman. All rights reserved.
 
-    public init(name: String, columns: [AnyColumn<ObjectType>], data: [ObjectType]) {
-        self.name = name
-        self.columns = columns
-        self.data = data
-    }
+import Foundation
 
-    // 便利构造器，支持直接传入 Column 数组
-    public init<C: Collection>(name: String, columns: C, data: [ObjectType])
-        where C.Element: ColumnProtocol, C.Element.ObjectType == ObjectType
-    {
-        self.name = name
-        self.columns = columns.map { AnyColumn($0) }
-        self.data = data
-    }
-}
+// public struct Sheet<ObjectType> {
+//     public let name: String
+//     public let columns: [AnyColumn<ObjectType>]
+//     public let data: [ObjectType]
+
+//     public init(name: String, columns: [AnyColumn<ObjectType>], data: [ObjectType]) {
+//         self.name = name
+//         self.columns = columns
+//         self.data = data
+//     }
+
+//     // 便利构造器，支持直接传入 Column 数组
+//     public init<C: Collection>(name: String, columns: C, data: [ObjectType])
+//         where C.Element: ColumnProtocol, C.Element.ObjectType == ObjectType
+//     {
+//         self.name = name
+//         self.columns = columns.map { AnyColumn($0) }
+//         self.data = data
+//     }
+// }
