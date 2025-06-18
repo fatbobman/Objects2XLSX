802266e5e9d9dd5033c4b63edbc214f532fa9bb1
fatbobman <xuyang@me.com>
2025-06-18 09:00:52 +0800

    Implement Book class with styles and sheets management; add ColumnBuilder, ColumnOutputTypeProtocol, and ColumnProtocol for enhanced column handling

--- Sources/Objects2XLSX/Core/Book/Book.swift
+++ Sources/Objects2XLSX/Core/Book/Book.swift
@@ -9,5 +9,31 @@
 import Foundation
 
 // 对应 Excel 的 Workbook 对象
-final class Book {
+public final class Book {
+    public var styles: BookStyle
+    public var sheets: [AnySheet]
+
+    let styleRegister: StyleRegister
+    let shareStringRegister: ShareStringRegister
+
+    public init(styles: BookStyle, sheets: [AnySheet] = []) {
+        self.styles = styles
+        self.sheets = sheets
+        styleRegister = StyleRegister()
+        shareStringRegister = ShareStringRegister()
+    }
+
+    public func append(sheet: AnySheet) {
+        sheets.append(sheet)
+    }
+
+    public func append(sheets: [AnySheet]) {
+        self.sheets.append(contentsOf: sheets)
+    }
+
+    public func write(to url: URL) throws {
+        // 生成逻辑
+        let data = Data()
+        try data.write(to: url)
+    }
 }
